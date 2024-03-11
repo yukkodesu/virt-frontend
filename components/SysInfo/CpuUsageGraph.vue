48bf22648eb98af54da432db954d33c7b578113f
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2024-03-11 17:27:04 +0800

    feat: uplift sysinfo state

--- components/SysInfo/CpuUsageGraph.vue
+++ components/SysInfo/CpuUsageGraph.vue
@@ -1,68 +1,54 @@
 <template>
     <div id="chart" class="py-8 px-4">
         <ClientOnly>
-            <apexchart type="line" height="350" :options="chartOptions" :series="series" @updated="onChartUpdated">
+            <apexchart type="line" height="350" :options="chartOptions" :series="series">
             </apexchart>
         </ClientOnly>
     </div>
 </template>
 
 <script setup lang="ts">
-import { inject } from 'vue';
+type SysInfo = {
+    "used swap": string;
+    "cpu number": string;
+    "cpu usage": string;
+    timestamp: string;
+    "used memory": string;
+    "total memory": string;
+    "total swap": string;
+};
+const props = defineProps<{
+    sysinfo: SysInfo
+}>();
 const series = ref<Array<{
     name: string,
     data: Array<Array<{ x: number, y: number }>>
 }>>([]);
 
-const showAlert = inject<(msg: string) => void>('showAlert');
-
-const { data, error: fetchError, refresh } = await useFetch('/api/sysinfo');
-if (fetchError.value) {
-    showAlert && showAlert(fetchError.value?.message);
+for (let i = 0; i < Number(props.sysinfo['cpu number']); i++) {
+    series.value.push({
+        name: `Core ${i}`,
+        data: []
+    })
 }
 
-const fetchData = async () => {
-    await refresh();
-    if (fetchError.value) {
-        showAlert && showAlert(fetchError.value?.message);
-    }
-    const sysinfo = data.value;
+const updateGraph = (sysinfo: SysInfo) => {
     const seriesVal = series.value;
-    if (seriesVal.length === 0) {
-        for (let i = 0; i < sysinfo['cpu number']; i++) {
-            seriesVal.push({
-                name: `Core ${i}`,
-                data: []
-            })
-        }
-    }
     const timestamp = Number(sysinfo['timestamp']);
     const usage = JSON.parse(sysinfo['cpu usage']);
     Object.entries(usage).forEach(([k, v]: [string, any]) => {
         const i = Number(k);
         seriesVal[i].data.push({ x: timestamp, y: v });
     });
-}
-
-let timer: any = null;
-
-onMounted(() => {
-    timer = setInterval(() => {
-        fetchData();
-    }, 2000);
-})
-
-onBeforeUnmount(() => {
-    clearInterval(timer);
-})
-
-const onChartUpdated = () => {
-    const sysinfo = data.value;
-    const seriesVal = series.value;
     if (seriesVal[0]?.data?.length <= 15) return;
     seriesVal.forEach(it => it.data.shift());
 }
 
+updateGraph(props.sysinfo);
+
+watch(props.sysinfo, (sysinfo) => {
+    updateGraph(sysinfo);
+})
 const chartOptions = ref({
     chart: {
         type: 'line',
--- pages/index/sysinfo.vue
+++ pages/index/sysinfo.vue
@@ -1,8 +1,45 @@
 <template>
     <div>
-        <SysInfoCpuUsageGraph />
+        <UCard>
+            <SysInfoCpuUsageGraph :sysinfo="sysinfo" />
+        </UCard>
     </div>
 </template>
 
 <script setup lang="ts">
+const showAlert = inject<(msg: string) => void>('showAlert');
+const sysinfo = ref({
+    "used swap": "",
+    "cpu number": "",
+    "cpu usage": "",
+    timestamp: "",
+    "used memory": "",
+    "total memory": "",
+    "total swap": "",
+});
+
+const { data, error: fetchError, refresh } = await useFetch('/api/sysinfo');
+
+const updateSysInfo = () => {
+    Object.entries(data.value).forEach(([k, v]: [string, any]) => {
+        (sysinfo.value)[k] = `${v}`;
+    })
+}
+
+if (fetchError.value) {
+    showAlert && showAlert(fetchError.value?.message);
+} else {
+    updateSysInfo();
+}
+
+let timer: any = null;
+onMounted(() => {
+    timer = setInterval(() => {
+        refresh();
+        updateSysInfo();
+    }, 2000);
+})
+onBeforeUnmount(() => {
+    clearInterval(timer);
+})
 </script>

0be49e65fbf7a47a6174e029d7817237e21c9686
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2024-03-19 11:02:05 +0800

    feat: snapshot list table half implemented

--- pages/index/snapshot/list.vue
+++ pages/index/snapshot/list.vue
@@ -1,9 +1,22 @@
 <template>
-    <div class="py-2">
-        <div class=" max-w-fit">
-            <USelectMenu v-model="selected" :options="options" />
+    <div class="py-2 flex flex-col gap-2">
+        <div class="flex gap-1">
+            <div class="flex items-center">
+                <span class="pr-2 text-sm">Select VM:</span>
+                <div class="max-w-fit">
+                    <USelectMenu v-model="selected" :options="options" />
+                </div>
+            </div>
         </div>
+        <UCard :ui="{
+                        body: {
+                            padding: 'sm:p-0'
+                        }
+                    }">
+            <UTable :rows="tableData" />
+        </UCard>
     </div>
+    {{ snapshotData }}
 </template>
 
 <script setup lang="ts">
@@ -14,5 +27,18 @@ await callOnce(updateDomains);
 const options = ref([]);
 const selected = ref("Loading");
 domains.map(it => options.value.push(it['name']));
-selected.value = (options.value)[0];
+selected.value = (options.value)[1];
+
+const { data: snapshotData, refresh } = useFetch("/api/list-snapshot", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify([selected.value]),
+});
+
+const tableData = computed(() => {
+    return (snapshotData.value)[selected.value];
+})
+
 </script>

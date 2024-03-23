6228cd75afecc202ae4b6313cb10223d2397c946
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2024-03-23 15:58:39 +0800

    feat: add loading animation

--- pages/index/snapshot/list.vue
+++ pages/index/snapshot/list.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="py-2 flex flex-col gap-2">
-        <div class="flex gap-1">
+        <div class="flex gap-4">
             <div class="flex items-center">
                 <span class="pr-2 text-sm">Select VM:</span>
                 <div class="max-w-fit">
                     <USelectMenu v-model="selected" :options="options" />
                 </div>
+            </div>
+            <div>
+                <UInput icon="i-heroicons-magnifying-glass-20-solid" size="sm" color="white" :trailing="false"
+                    v-model="searchInput" placeholder="Search..." />
             </div>
         </div>
         <UCard :ui="{
@@ -13,18 +17,39 @@
                             padding: 'sm:p-0'
                         }
                     }">
-            <UTable :rows="tableData" :columns="col" />
+            <UTable :rows="tableData" :columns="col" :loading="isTableLoading"
+                :loading-state="{ icon: 'i-heroicons-arrow-path-20-solid', label: 'Loading...' }"
+                :progress="{ color: 'primary', animation: 'carousel' }">
+                <template #name-data="{ row }">
+                    <span :class="[row['isCurrent'] && 'text-primary-500 dark:text-primary-400']">
+                        {{ row['isCurrent'] ? `${row.name} (Current)` : row.name }}
+                    </span>
+                </template>
+                <template #actions-data="{ row }">
+                    <UDropdown :items="items(row)">
+                        <UButton color="gray" variant="ghost" icon="i-heroicons-ellipsis-horizontal-20-solid" />
+                    </UDropdown>
+                </template>
+            </UTable>
         </UCard>
     </div>
+    <SnapshotEditor v-model:is-open="isOpenEditModel" v-model:snapshot-info="modelSnapshotInfo"
+        @submit="refreshSnapshotData" />
 </template>
 
 <script setup lang="ts">
 import { useVirtStore } from '~/store/virt';
+import dayjs from 'dayjs';
+import localizedFormat from "dayjs/plugin/localizedFormat";
+dayjs.extend(localizedFormat);
+
+
 const virtStore = useVirtStore();
 const { domains, updateDomains } = virtStore;
 await callOnce(updateDomains);
 const options = ref([]);
 const selected = ref("Loading");
+const isTableLoading = ref(true);
 domains.map(it => options.value.push(it['name']));
 selected.value = (options.value)[0];
 
@@ -54,19 +79,93 @@ const col = [
         label: "Description",
     },
     {
-        key: "actions"
+        key: "actions",
+        label: "Action"
     }
+];
+
+const isOpenEditModel = ref(false);
+const modelSnapshotInfo = ref<{ name: string, description: string }>({
+    name: "",
+    description: "",
+});
+
+const items = (row) => [
+    [
+        {
+            label: 'Edit',
+            icon: 'i-heroicons-pencil-square-20-solid',
+            click: () => {
+                isOpenEditModel.value = true;
+                modelSnapshotInfo.value = {
+                    name: row['name'],
+                    description: row['description'] === "None" ? "" : row['description']
+                }
+            },
+        },
+        {
+            label: 'Set Current',
+            icon: 'i-heroicons-check-circle-20-solid',
+            click: async () => {
+                isTableLoading.value = true;
+                await $fetch('/api/set-current-snapshot', {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    method: "POST",
+                    body: {
+                        dom_name: selected.value,
+                        snapshot_name: row['name'],
+                    },
+                });
+                await refreshSnapshotData();
+                isTableLoading.value = false;
+            },
+        },
+        {
+            label: 'Clone',
+            icon: 'i-heroicons-document-duplicate-20-solid'
+        },
+        {
+            label: 'Delete',
+            icon: 'i-heroicons-trash-20-solid'
+        }
+    ]
 ]
 
-refreshSnapshotData()
+refreshSnapshotData().then(() => {
+    isTableLoading.value = false;
+});
 
-watch(selected, () => {
-    refreshSnapshotData();
+watch(selected, async () => {
+    isTableLoading.value = true;
+    await refreshSnapshotData();
+    isTableLoading.value = false;
 })
+
+const searchInput = ref("");
 
 const tableData = computed(() => {
-    if(!snapshotData.value) return [];
-    return (snapshotData.value)[selected.value];
-})
+    if (!snapshotData.value) return [];
+    const data = (snapshotData.value)[selected.value]?.map(it => {
+        return {
+            ...it,
+            isCurrent: it['isCurrent'].toLowerCase() === 'true' ? true : false,
+            creationTime: dayjs(Number(it['creationTime']) * 1000).format('llll'),
+            description: it['description'] === "" ? "None" : it['description'],
+        };
+    });
+    if (searchInput.value !== "") {
+        return data?.filter(it => {
+            for (let val of Object.values(it)) {
+                if (`${val}`.toLowerCase().includes(searchInput.value)) {
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+    return data;
+});
 
 </script>

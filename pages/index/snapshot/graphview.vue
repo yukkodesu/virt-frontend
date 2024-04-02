68dc6057af23111dca24f168a1f9349116d7c9bd
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2024-04-02 10:06:15 +0800

    feat: graphview data fetch

--- pages/index/snapshot/graphview.vue
+++ pages/index/snapshot/graphview.vue
@@ -1,61 +1,40 @@
 <script setup lang="ts">
 import type { Elements } from '@vue-flow/core'
 import { VueFlow } from '@vue-flow/core'
+const virtStore = useVirtStore();
+const { domains, updateDomains } = virtStore;
+await callOnce(updateDomains);
+const options = ref([]);
+const selected = ref("Loading");
+domains.map(it => options.value.push(it['name']));
+selected.value = (options.value)[0];
 
-const elements = ref<Elements>([
-  // nodes
-
-  // an input node, specified by using `type: 'input'`
-  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
-
-  // default node, you can omit `type: 'default'` as it's the fallback type
-  { id: '2', label: 'Node 2', position: { x: 100, y: 100 }, },
-
-  // An output node, specified by using `type: 'output'`
-  { id: '3', type: 'output', label: 'Node 3', position: { x: 400, y: 200 } },
-
-  // A custom node, specified by using a custom type name
-  // we choose `type: 'special'` for this example
-  {
-    id: '4',
-    type: 'special',
-    label: 'Node 4',
-    position: { x: 400, y: 200 },
-
-    // pass custom data to the node
-    data: {
-      // you can pass any data you want to the node
-      hello: 'world',
-    },
+const { data: snapshotTree, refresh: refreshSnapshotTree } = useAsyncData("snapshots", () => $fetch('/api/list-snapshot-tree', {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
   },
+  body: JSON.stringify(selected.value),
+}));
+const elements = ref<Elements>([]);
 
-  // edges
+refreshSnapshotTree().then(() => {
+  console.log(snapshotTree.value);
+});
 
-  // simple default bezier edge
-  // consists of an id, source-id and target-id
-  { id: 'e1-3', source: '1', target: '3' },
-
-  // an animated edge, specified by using `animated: true`
-  { id: 'e1-2', source: '1', target: '2', animated: true },
-
-  // a custom edge, specified by using a custom type name
-  // we choose `type: 'special'` for this example
-  {
-    id: 'e1-4',
-    type: 'special',
-    source: '1',
-    target: '4',
-
-    // pass custom data to the edge
-    data: {
-      // You can pass any data you want to the edge       
-      hello: 'world',
-    }
-  },
-])
 </script>
 
 <template>
+  <div class="py-2 flex flex-col gap-2">
+    <div class="flex gap-4">
+      <div class="flex items-center">
+        <span class="pr-2 text-sm">Select VM:</span>
+        <div class="max-w-fit">
+          <USelectMenu v-model="selected" :options="options" />
+        </div>
+      </div>
+    </div>
+  </div>
   <VueFlow v-model="elements">
   </VueFlow>
 </template>

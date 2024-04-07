1dbbbe86e9304386cb5296492bb31e6fc6aff8cd
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2024-04-07 11:16:10 +0800

    feat: implement graphview toposort node positioning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
     },
   },
   pinia: {
-    storesDirs: ['./store/**'],
+    storesDirs: ["./store/**"],
   },
   routeRules: {
     "/api/**": {
--- pages/index/snapshot/graphview.vue
+++ pages/index/snapshot/graphview.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import type { Elements } from '@vue-flow/core'
 import { VueFlow } from '@vue-flow/core'
+
+const el = ref(null);
+
 const virtStore = useVirtStore();
 const { domains, updateDomains } = virtStore;
 await callOnce(updateDomains);
+
 const options = ref([]);
 const selected = ref("Loading");
 domains.map(it => options.value.push(it['name']));
@@ -16,11 +20,84 @@ const { data: snapshotTree, refresh: refreshSnapshotTree } = useAsyncData("snaps
   },
   body: JSON.stringify(selected.value),
 }));
+
 const elements = ref<Elements>([]);
 
+const generatePositions = function (edges: Ref<{ [idx: string]: string[] }>, maxWidth: number, stepHeight: number): { [idx: string]: { x: number, y: number } } {
+  const obj = edges.value;
+  const positions: { [idx: string]: { x: number, y: number } } = {};
+  const S = [];
+  const queue: Array<{
+    node: string,
+    parent: string | null,
+    level: number,
+    position: { x: number, y: number }
+  }> = [];
+  const inBound = new Map<string, number>();
+  Object.entries(obj).forEach(([_, v]) => {
+    v.forEach((it) => {
+      let prev = inBound.get(it);
+      inBound.set(it, prev ? prev + 1 : 1);
+    });
+  });
+  for (let k of Object.keys(obj)) {
+    if (!inBound.get(k)) S.push(k);
+  }
+  S.forEach((it, idx, arr) => {
+    const offset = arr.length > 1 ? maxWidth / (arr.length - 1) : 0;
+    const x = idx * offset - ((arr.length - 1) * offset) / 2;
+    queue.push({
+      node: it,
+      parent: null,
+      level: 0,
+      position: { x, y: 0 },
+    });
+  });
+  while (queue.length > 0) {
+    const u = queue.shift();
+    if (!u) continue;
+    const { node, parent, level, position } = u;
+    const children = obj[node] || [];
+    positions[node] = { ...position };
+
+    if (parent) {
+      const parentPosition = positions[parent];
+      const siblingCount = obj[parent].length;
+      const offset = siblingCount > 1 ? maxWidth / (siblingCount - 1) : 0;
+      const siblingIndex = obj[parent].indexOf(node);
+      const x =
+        parentPosition.x +
+        (siblingIndex * offset - ((siblingCount - 1) * offset) / 2);
+      const y = parentPosition.y + stepHeight;
+      positions[node].x = x;
+      positions[node].y = y;
+    }
+
+    for (const child of children) {
+      queue.push({ node: child, parent: node, level: level + 1, position: { x: 0, y: 0 } });
+    }
+  }
+
+  return positions;
+}
+
+
 refreshSnapshotTree().then(() => {
-  console.log(snapshotTree.value);
+
 });
+
+watch(selected, async () => {
+  await refreshSnapshotTree();
+  const pos = generatePositions(snapshotTree, 300, 100);
+  console.log(pos);
+  for (let edge in snapshotTree.value) {
+    elements.value.push({ id: edge, label: edge, position: { x: pos[edge].x + 200, y: pos[edge].y } });
+    (snapshotTree.value)[edge].forEach(it => {
+      elements.value.push({ id: `e${edge}-${it}`, source: `${edge}`, target: `${it}` });
+    })
+  }
+  console.log(elements.value);
+})
 
 </script>
 
@@ -35,8 +112,10 @@ refreshSnapshotTree().then(() => {
       </div>
     </div>
   </div>
-  <VueFlow v-model="elements">
-  </VueFlow>
+  <div ref="el" class="w-full h-screen">
+    <VueFlow v-model="elements">
+    </VueFlow>
+  </div>
 </template>
 
 <style>

cc63a0c3e53d3521facb5f777639589c29d0e0aa
yukkodesu <17264509+yukkodesu@users.noreply.github.com>
2024-04-15 16:49:09 +0800

    feat: polish virt page

--- pages/index/vm/[...idx].vue
+++ pages/index/vm/[...idx].vue
@@ -1,36 +1,109 @@
 <template>
-    <UCard>
-        <template #header>
-            <div class="flex justify-between">
-                <h2 class="text-md font-semibold">{{ route.path.split('/').at(2) }}</h2>
-                
-            </div>
-        </template>
-        <template #default>
-            <div class="flex gap-[100px] items-center">
-                <div>
-                    <img :src="`/distro_icons/512/512_debian.png`" alt="os icon" class="h-[80px] w-[80px]">
-                </div>
-                <div class="text-sm/6 text-gray-500">
-                    <ul class="grid grid-cols-2 gap-x-[100px] gap-y-4">
-                        <li>Name: debian</li>
-                        <li>CPU Numbers: 4</li>
-                        <li>Memory Size: 8GB</li>
-                        <li>State: shutoff</li>
+    <div class="flex flex-col gap-4">
+        <UCard>
+            <template #header>
+                <div class="flex justify-between">
+                    <h2 class="text-md font-semibold">{{ route.path.split('/').at(2) }}</h2>
+                    <ul class="flex gap-2">
+                        <li v-for="item in items" :key="item.label">
+                            <UButton :icon="item.icon" variant="solid" @click="item.click">{{ item.label }}</UButton>
+                        </li>
                     </ul>
                 </div>
-            </div>
-        </template>
-    </UCard>
+            </template>
+            <template #default>
+                <div class="flex items-center gap-12 px-6 py-0">
+                    <div>
+                        <img :src="`/distro_icons/512/512_debian.png`" alt="os icon" class="h-[60px] w-[60px]">
+                    </div>
+                    <div class="text-sm/6 text-gray-500 px-2">
+                        <ul class="grid grid-cols-2 gap-x-[100px] gap-y-4">
+                            <li>Name: {{ domain?.name }}</li>
+                            <li>CPU Numbers: {{ domain?.vcpu }}</li>
+                            <li>RAM Size: {{ formatBytes(Number(domain?.memory) * 1024) }}</li>
+                            <li>State: {{ virtState[Number(domain?.state)] }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </template>
+        </UCard>
+        <UCard>
+            <template #default>
+                <div class="w-full aspect-[3/2] bg-slate-800">
+                    123
+                </div>
+            </template>
+        </UCard>
+        <AlertDialog v-model:is-open="isAlertOpen" :msg="alertMsg" :on-confirm="onAlertComfirm" />
+    </div>
 </template>
 
 <script setup lang="ts">
-const route = useRoute();
+import { virtState, formatBytes } from '~/components/VM/utils';
 
+const route = useRoute();
 useHead({
     title: () => `${route.path.split('/').at(2)} - Virt Domains`,
 });
 definePageMeta({
     title: 'Virt Domains',
 });
+
+const virtStore = useVirtStore();
+const { domains, updateDomains } = virtStore;
+
+const domain = computed(() => domains.find(it => it.name === `${route.path.split('/').at(2)}`));
+
+const setDomState = async (dom_name: string | undefined, state: string) => {
+    if (!dom_name) return;
+    await $fetch('/api/alt-vm-state', {
+        method: 'POST',
+        body: {
+            dom_name,
+            state,
+        }
+    });
+    await updateDomains();
+}
+
+
+const items =
+    [
+        {
+            label: 'Start',
+            icon: 'i-heroicons-play-20-solid',
+            click: setDomState.bind(undefined, domain.value?.name, "start"),
+        },
+        {
+            label: 'Shutdown',
+            icon: 'i-heroicons-stop-20-solid',
+            click: setDomState.bind(undefined, domain.value?.name, "shutdown"),
+        },
+        {
+            label: 'Suspend',
+            icon: 'i-heroicons-pause-20-solid',
+            click: setDomState.bind(undefined, domain.value?.name, "suspend"),
+        },
+        {
+            label: 'Destroy',
+            icon: 'i-tabler-plug-x',
+            click: () => {
+                openAlert("Do you want to cut power of your VM? This action could cause issue in VM.");
+                onAlertComfirm.value = async () => {
+                    await setDomState(domain.value?.name, "destroy");
+                    isAlertOpen.value = false;
+                };
+            },
+        },
+    ];
+
+//Alert Dialog
+const isAlertOpen = ref(false);
+const alertMsg = ref('');
+const onAlertComfirm = ref<(() => Promise<void>) | null>(null);
+const openAlert = (msg: string) => {
+    alertMsg.value = msg;
+    isAlertOpen.value = true;
+};
+
 </script>
